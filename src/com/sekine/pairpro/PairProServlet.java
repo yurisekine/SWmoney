66cc405a67d7f44244dbcb27fa1063b4b89c9925
g14944sy <g14944sy@s20618.tsuda.ac.jp>
2016-06-02 18:46:23 +0900

    Third

--- src/com/sekine/pairpro/PairProServlet.java
+++ src/com/sekine/pairpro/PairProServlet.java
@@ -36,26 +36,26 @@ public class PairProServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        HttpSession session = request.getSession();
+    /*    HttpSession session = request.getSession();
         RequestDispatcher dispatcher = request.getRequestDispatcher("/top.jsp");
-        dispatcher.forward(request, response);
+        dispatcher.forward(request, response);*/
         
         PersistenceManagerFactory factory = PMF.get();
         PersistenceManager manager = factory.getPersistenceManager();
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html");
         request.setCharacterEncoding("utf-8");
-        String day = request.getParameter("day");
+        String param1 = request.getParameter("id");
         PrintWriter out = response.getWriter();
         List<Data> list = null;
-        if (day == null || day ==""){
+        if (param1 == null || param1 ==""){
             String query = "select from " + Data.class.getName();
             try {
                 list = (List<Data>)manager.newQuery(query).execute();
             } catch(JDOObjectNotFoundException e){}
         } else {
             try {
-                Data data = (Data)manager.getObjectById(Data.class,Long.parseLong(day));
+                Data data = (Data)manager.getObjectById(Data.class,Long.parseLong(param1));
                 list = new ArrayList();
                 list.add(data);
             } catch(JDOObjectNotFoundException e){}
@@ -63,7 +63,7 @@ public class PairProServlet extends HttpServlet {
         String res = "[";
         if (list != null){
             for(Data data:list){
-                res += "{day:" + data.getDay() + ",time:'" + data.getTime() + "'},";
+                res += "{id:" + data.getId() + ",day:" + data.getDay() + ",time:'" + data.getTime() + "'},";
             }
         }
         res += "]";
